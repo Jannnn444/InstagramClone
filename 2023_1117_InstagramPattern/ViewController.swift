3efcf0d83d731824571c4b9ae18517385f686b58
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2023-11-17 02:20:53 +0800

    Initial Commit

---
+++ 2023_1117_InstagramPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2023_1117_InstagramPattern
+//
+//  Created by yucian huang on 2023/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
